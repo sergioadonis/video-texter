6d577675372a6362681ab9342fff063761ed7c35
Sergio Adonis <sergio.adonis@hotmail.com>
2020-06-27 18:56:10 -0600

    base template using semantic-ui

--- video_texter/urls.py
+++ video_texter/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home_index'),
+    path('', TemplateView.as_view(template_name='index.html', extra_context={'title': 'Home'}), name='home_index'),
     path('admin/', admin.site.urls),
 ]
