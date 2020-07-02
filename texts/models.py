6e456c0d25dc949599ca1991014ac2f5e65cf12d
Sergio Adonis <sergio.adonis@hotmail.com>
2020-07-02 17:39:23 +0000

    adding json data field in YoutubeLink model instead of title and language code

--- texts/models.py
+++ texts/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 
 class YoutubeLink(models.Model):
     link = models.CharField(max_length=50)
-    title = models.CharField(max_length=50, default='', blank=True)
-    language_code = models.CharField(max_length=5, default='', blank=True)
+    data = JSONField(default=dict, blank=True)
+#    title = models.CharField(max_length=50, default='', blank=True)
+#    language_code = models.CharField(max_length=5, default='', blank=True)
 
     def __str__(self):
         return self.title if self.title else self.link
+
+    @property
+    def title(self):
+        try:
+            return self.data['snippet']['title']
+        except:
+            return ''
+
+    @property
+    def language_code(self):
+        try:
+            return self.data['snippet']['defaultAudioLanguage']
+        except:
+            return ''
 
     def must_update_title_or_language_code(self):
         if len(self.link) == 0:
