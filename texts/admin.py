75cdb704a1e5b1716972641f64166d54bb6aecc8
Sergio Adonis <sergio.adonis@hotmail.com>
2020-07-05 16:42:22 +0000

    spliting models

--- texts/admin.py
+++ texts/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from .models import YoutubeVideo
+from .models import YoutubeVideo, YoutubeVideoTask
 from .tasks import import_youtubevideo_data, update_youtubevideo_data
 
 
 @admin.register(YoutubeVideo)
 class YoutubeLinkAdmin(admin.ModelAdmin):
-    list_display = ('url', 'title', 'language_code', 'task_id')
+    list_display = ('url', 'title', 'language_code')
     actions = ['import_data']
 
     def import_data(self, request, queryset):
+        tasks = []
         for obj in queryset:
             url = obj.url
             pk = obj.pk
             task_id = import_youtubevideo_data.apply_async(args=(url,), link=update_youtubevideo_data.s(pk))
-            obj.task_id = task_id
-        YoutubeVideo.objects.bulk_update(list(queryset), ['task_id'])
+            task = YoutubeVideoTask(youtubevideo=obj, task_id=task_id) 
+            tasks.append(task)
+        YoutubeVideoTask.objects.bulk_create(tasks)
 
     import_data.short_description = 'Import data from Youtube'
 
--- texts/models.py
+++ texts/models.py
@@ -4,7 +4,6 @@ from django.contrib.postgres.fields import JSONField
 
 class YoutubeVideo(models.Model):
     url = models.CharField(max_length=50)
-    task_id = models.CharField(max_length=255, blank=True, default='')
     data = JSONField(default=dict, blank=True)
 
     def __str__(self):
@@ -23,3 +22,12 @@ class YoutubeVideo(models.Model):
             return self.data['snippet']['defaultAudioLanguage']
         except:
             return ''
+
+
+class YoutubeVideoTask(models.Model):
+    youtubevideo = models.ForeignKey(YoutubeVideo, on_delete=models.CASCADE, related_name='tasks')
+    task_id = models.CharField(max_length=255, blank=True, default='')
+    
+    def __str__(self):
+        return self.task_id
+
